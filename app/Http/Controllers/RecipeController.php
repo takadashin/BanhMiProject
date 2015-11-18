8f435fb6050367c54b7cb2de94ab6828c928391e
takadashin <khaivo17@yahoo.com>
2015-11-17 20:52:54 -0500

    add complete homepage

--- app/Http/Controllers/RecipeController.php
+++ app/Http/Controllers/RecipeController.php
@@ -1,14 +1,23 @@
 <?php
 namespace App\Http\Controllers;
-
+use DB;
 use App\Recipe;
 use Illuminate\Http\Request;
 
 
 class RecipeController extends Controller {
     public function  index(){
-        $cds = Recipe::all();
-        return view('pages.index')->with('recipe',$cds);
+        $cds =  DB::select('SELECT recipe.*,user.avatar,user.username,'
+                . ' (select COUNT(follow.id) from follow WHERE follow.userid = userpostid) as countfollow, '
+                . '(select COUNT(made.id) from made WHERE made.userid = userpostid) as countmade, '
+                . '(select COUNT(vote.id) from vote WHERE vote.userid = userpostid and likes = true) as countlike '
+                . 'FROM recipe,user where recipe.userpostid = user.id order by recipe.datepost LIMIT 16');
+        $db =  DB::select('SELECT recipe.*,user.avatar,user.username,'
+                . ' (select COUNT(follow.id) from follow WHERE follow.userid = userpostid) as countfollow, '
+                . '(select COUNT(made.id) from made WHERE made.userid = userpostid) as countmade, '
+                . '(select COUNT(vote.id) from vote WHERE vote.userid = userpostid and likes = true) as countlike '
+                . 'FROM recipe,user where recipe.userpostid = user.id order by countlike LIMIT 16');
+        return view('pages.index', ['recipe' => $cds,'popular' => $db]);
     }
     
     
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -12,7 +12,7 @@
         </div>
         <div>
              @foreach($recipe as $row)
-            <div class="article">
+             <div style="cursor: pointer;" class="article" onclick="location.href='{{ url('/recipe', $row->id) }}';">
                 <center>
                     <div>
                         <img src="{{ asset('assets/images/article_pic/'.$row->img) }}" />
@@ -24,12 +24,12 @@
                     </div>
                     <div><hr></div>
                     <div class="article_content foot_content" >
-                        <a href="http://google.com"><img src="{{ asset('assets/images/user-group-512.png') }}"  style="width: 15px;height:15px;vertical-align: -3px;" /> 42</a>
-                        <a href="http://google.com"><img src="{{ asset('assets/images/spoon.jpg') }}"  style="width: 15px;height:15px;vertical-align: -3px;" /> 105</a>
-                        <a href="http://google.com"><img src="{{ asset('assets/images/thumpup.png') }}"  style="width: 15px;height:15px;vertical-align: -3px;" /> 42</a>
+                        <a href="http://google.com"><img src="{{ asset('assets/images/user-group-512.png') }}"  style="width: 15px;height:15px;vertical-align: -3px;" /> {{$row->countfollow}}</a>
+                        <a href="http://google.com"><img src="{{ asset('assets/images/spoon.jpg') }}"  style="width: 15px;height:15px;vertical-align: -3px;" /> {{$row->countmade}}</a>
+                        <a href="http://google.com"><img src="{{ asset('assets/images/thumpup.png') }}"  style="width: 15px;height:15px;vertical-align: -3px;" /> {{$row->countlike}}</a>
 
                         <div style="margin-top:10px;text-align: right;">Made by 
-                            <a href="http://google.com"><img src="{{ asset('assets/images/mystery_person.png') }}"  style="width: 46px;height:auto;vertical-align: -19px;" /> </a></div>
+                            <a href="{{ url('/user', $row->userpostid) }}"><img src="{{ asset('assets/images/user_pic/'.$row->avatar) }}"  style="width: 46px;height:auto;vertical-align: -19px;" /> </a></div>
                     </div>
                 </center>
             </div>
@@ -38,100 +38,52 @@
             <div class="clear" ></div>
 
         </div>
-<!--        <div class="header_title">
+        <div class="header_title">
             <h1>POPULAR RECIPE</h1>
         </div>
+        <?php $count = 0; ?>
         <div>
-            <div class="article top_place">
-                <img src="{{ asset('assets/images/first.png') }}" class="medal" />
+             @foreach($popular as $row)
+             <div style="cursor: pointer;" class="article" onclick="location.href='{{ url('/recipe', $row->id) }}';">
+                 <?php
+                 $stringimg="";
+                 $count++;
+                 if($count <= 3)
+                 {
+                    if($count == 1)
+                        $stringimg="first.png";
+                    else if($count == 2)
+                        $stringimg = "second.png";
+                    else if($count == 3)
+                        $stringimg = "third.png";
+                    echo "<img src=". asset('assets/images/'.$stringimg) ." class='medal' />";
+                 }
+                 ?>
                 <center>
                     <div>
-                        <img src="{{ asset('assets/images/article_pic/1826.jpg') }}" />
+                        <img src="{{ asset('assets/images/article_pic/'.$row->img) }}" />
                     </div>
 
                     <div class="article_content mid_content" >
-                        <h3>Italian Meatball Sandwich Casserole</h3>
-                        <p>"All the ingredients for a meatball sandwich are here, just assembled in a different manner. This recipe is always a hit at our house. We NEVER have any leftovers, it is so good!"</p>
+                        <h3>{{$row->name}}</h3>
+                        <p>{{$row->Description}}</p>
                     </div>
                     <div><hr></div>
                     <div class="article_content foot_content" >
-                        <a href="http://google.com"><img src="{{ asset('assets/images/user-group-512.png') }}"  style="width: 15px;height:15px;vertical-align: -3px;" /> 42</a>
-                        <a href="http://google.com"><img src="{{ asset('assets/images/spoon.jpg') }}"  style="width: 15px;height:15px;vertical-align: -3px;" /> 105</a>
-                        <a href="http://google.com"><img src="{{ asset('assets/images/thumpup.png') }}"  style="width: 15px;height:15px;vertical-align: -3px;" /> 42</a>
+                        <a href="http://google.com"><img src="{{ asset('assets/images/user-group-512.png') }}"  style="width: 15px;height:15px;vertical-align: -3px;" /> {{$row->countfollow}}</a>
+                        <a href="http://google.com"><img src="{{ asset('assets/images/spoon.jpg') }}"  style="width: 15px;height:15px;vertical-align: -3px;" /> {{$row->countmade}}</a>
+                        <a href="http://google.com"><img src="{{ asset('assets/images/thumpup.png') }}"  style="width: 15px;height:15px;vertical-align: -3px;" /> {{$row->countlike}}</a>
 
                         <div style="margin-top:10px;text-align: right;">Made by 
-                            <a href="http://google.com"><img src="{{ asset('assets/images/mystery_person.png') }}"  style="width: 46px;height:auto;vertical-align: -19px;" /> </a></div>
+                            <a href="{{ url('/user', $row->userpostid) }}"><img src="{{ asset('assets/images/user_pic/'.$row->avatar) }}"  style="width: 46px;height:auto;vertical-align: -19px;" /> </a></div>
                     </div>
                 </center>
             </div>
-            <div class="article">
-                <img src="{{ asset('assets/images/second.png') }}" class="medal" />
-                <center>
-                    <div>
-                        <img src="{{ asset('assets/images/article_pic/566024.jpg') }}" />
-                    </div>
-
-                    <div class="article_content mid_content" >
-                        <h3>Cheesy Ham and Hash Brown Casserole</h3>
-                        <p>"I mostly use this as a breakfast casserole, but it's great anytime. May be served with or without diced ham. Quick and easy to make, not to mention delicious!"</p>
-                    </div>
-                    <div><hr></div>
-                    <div class="article_content foot_content" >
-                        <a href="http://google.com"><img src="{{ asset('assets/images/user-group-512.png') }}"  style="width: 15px;height:15px;vertical-align: -3px;" /> 42</a>
-                        <a href="http://google.com"><img src="{{ asset('assets/images/spoon.jpg') }}"  style="width: 15px;height:15px;vertical-align: -3px;" /> 105</a>
-                        <a href="http://google.com"><img src="{{ asset('assets/images/thumpup.png') }}"  style="width: 15px;height:15px;vertical-align: -3px;" /> 42</a>
-
-                        <div style="margin-top:10px;text-align: right;">Made by 
-                            <a href="http://google.com"><img src="{{ asset('assets/images/mystery_person.png') }}"  style="width: 46px;height:auto;vertical-align: -19px;" /> </a></div>
-                    </div>
-                </center>
-            </div>
-            <div class="article">
-                <img src="{{ asset('assets/images/third.png') }}" class="medal" />
-                <center>
-                    <div>
-                        <img src="{{ asset('assets/images/article_pic/788831.jpg') }}" />
-                    </div>
-
-                    <div class="article_content mid_content" >
-                        <h3>Pumpkin Waffles with Apple Cider Syrup</h3>
-                        <p>"A fall, winter and special occasion family favorite, these are fairly easy to make and delicious! They're sure to please even picky eaters."</p>
-                    </div>
-                    <div><hr></div>
-                    <div class="article_content foot_content" >
-                        <a href="http://google.com"><img src="{{ asset('assets/images/user-group-512.png') }}"  style="width: 15px;height:15px;vertical-align: -3px;" /> 42</a>
-                        <a href="http://google.com"><img src="{{ asset('assets/images/spoon.jpg') }}"  style="width: 15px;height:15px;vertical-align: -3px;" /> 105</a>
-                        <a href="http://google.com"><img src="{{ asset('assets/images/thumpup.png') }}"  style="width: 15px;height:15px;vertical-align: -3px;" /> 42</a>
-
-                        <div style="margin-top:10px;text-align: right;">Made by 
-                            <a href="http://google.com"><img src="{{ asset('assets/images/mystery_person.png') }}"  style="width: 46px;height:auto;vertical-align: -19px;" /> </a></div>
-                    </div>
-                </center>
-            </div>
-            <div class="article">
-                <center>
-                    <div>
-                        <img src="{{ asset('assets/images/article_pic/1826.jpg') }}" />
-                    </div>
-
-                    <div class="article_content mid_content" >
-                        <h3>Italian Meatball Sandwich Casserole</h3>
-                        <p>"All the ingredients for a meatball sandwich are here, just assembled in a different manner. This recipe is always a hit at our house. We NEVER have any leftovers, it is so good!"</p>
-                    </div>
-                    <div><hr></div>
-                    <div class="article_content foot_content" >
-                        <a href="http://google.com"><img src="{{ asset('assets/images/user-group-512.png') }}"  style="width: 15px;height:15px;vertical-align: -3px;" /> 42</a>
-                        <a href="http://google.com"><img src="{{ asset('assets/images/spoon.jpg') }}"  style="width: 15px;height:15px;vertical-align: -3px;" /> 105</a>
-                        <a href="http://google.com"><img src="{{ asset('assets/images/thumpup.png') }}"  style="width: 15px;height:15px;vertical-align: -3px;" /> 42</a>
-
-                        <div style="margin-top:10px;text-align: right;">Made by 
-                            <a href="http://google.com"><img src="{{ asset('assets/images/mystery_person.png') }}"  style="width: 46px;height:auto;vertical-align: -19px;" /> </a></div>
-                    </div>
-                </center>
-            </div>
+            @endforeach
+      
             <div class="clear" ></div>
 
-        </div>-->
+        </div>
     </div>
 
 @stop
