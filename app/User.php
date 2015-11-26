20676384929af2071bee2a0f8d019da3bcd71a89
linhngoc1841 <linhngoc1841@yahoo.com>
2015-11-26 10:17:17 -0500

    login twitter

--- app/User.php
+++ app/User.php
@@ -21,19 +21,20 @@ class User extends Model implements AuthenticatableContract,
      *
      * @var string
      */
-    protected $table = 'users';
+    protected $table = 'user';
+    public  $timestamps = false;
 
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
-    protected $fillable = ['name', 'email', 'password'];
+    protected $fillable = ['username', 'email', 'password', 'address','phone','role','avatar'];
 
     /**
      * The attributes excluded from the model's JSON form.
      *
      * @var array
      */
-    protected $hidden = ['password', 'remember_token'];
+    protected $hidden = ['password'];
 }
