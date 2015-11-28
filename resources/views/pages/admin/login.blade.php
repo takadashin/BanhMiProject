32f6cddbcb42a68341109b5b1fe7aacb00fbfb52
linhngoc1841 <linhngoc1841@yahoo.com>
2015-11-27 23:21:08 -0500

    login twitter

--- resources/views/pages/admin/login.blade.php
+++ resources/views/pages/admin/login.blade.php
@@ -16,19 +16,21 @@ and open the template in the editor.
             <div style="position: relative;">
                 <img src="{{ asset('assets/images/logo.png') }}" style="position: absolute; margin-left: -225px;" >
                 <div style="padding:55px;background: #B0B8D4;">
-                    <table>
-                        <tr>
-                            <td>User Name</td>
-                            <td>:</td>
-                            <td><input class="login_text" type="text" name="username" /> </td>
-                        </tr>
-                        <tr>
-                            <td>Password</td>
-                            <td>:</td>
-                            <td><input class="login_text" type="password" name="username" /> </td>
-                        </tr>
-                    </table>
-                    <input type="submit" class="submitbutton" name="btn_submit" value="Log in" />
+                    {!! Form::open(['route' => 'sessions.store']) !!}
+                        <table>
+                            <tr>
+                                <td>User Name</td>
+                                <td>:</td>
+                                <td> {!! Form::text('username',null,array('class' => 'login_text')) !!} </td>
+                            </tr>
+                            <tr>
+                                <td>Password</td>
+                                <td>:</td>
+                                <td>{!! Form::password('password',array('class' => 'login_text')) !!} </td>
+                            </tr>
+                        </table>
+                        {!! Form::submit('Log In',array('class' => 'submitbutton')) !!}
+                     {!! Form::close() !!}
                 </div>
             </div>
         </div>
