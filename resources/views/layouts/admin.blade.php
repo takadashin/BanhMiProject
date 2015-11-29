f935944d1ce5e2b46108cf52d460e7ad0375e13f
linhngoc1841 <linhngoc1841@yahoo.com>
2015-11-28 23:04:11 -0500

    login twitter

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -2,13 +2,14 @@
     <head>
         <title>@yield('title')</title>
         <meta charset="UTF-8">
+        <meta name="csrf-token" content="{!! csrf_token() !!}">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="{{ asset("assets/css/admin.css") }}" rel="stylesheet">
         <link rel="icon" type="image/ico" href="{{ asset('assets/images/favicon.ico') }}">
         <script src="{{ asset("assets/ckeditor/ckeditor.js") }}" type="text/javascript"></script>
     </head>
     <body>
-        <div>
+        <div id="body">
             <div id="control_panel">
                 <div id="user_stat">
                     @if(Auth::check())
@@ -19,7 +20,8 @@
                 <div id="control_box">
                     <ul>
                         <li class="active"><a>Recipe Post</a></li>
-                        <li><a>User</a></li>                       
+                        <li><a>User</a></li> 
+                        <li><a href="ingredient">Ingredient</a></li> 
                     </ul>
                 </div>
             </div>
