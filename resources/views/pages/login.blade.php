4a723955a5c8d9618dfb84a72c8f5158c94e3916
takadashin <khaivo17@yahoo.com>
2015-12-04 16:25:54 -0500

    add css login
    Signed-off-by: takadashin <khaivo17@yahoo.com>

--- resources/views/pages/login.blade.php
+++ resources/views/pages/login.blade.php
@@ -10,16 +10,16 @@
     <div class="login-error-msg">{{ Session::get('login_error') }} </div>
        {!! Form::open(['route' => 'sessions.store']) !!}
     <div class="login-field-space">
-        {!! Form::label('username', 'Username : ') !!}
-        {!! Form::text('username') !!}     
+        {!! Form::label('username', 'Username : ',['class'=>'label_search','style'=>'width:80px;']) !!}
+        {!! Form::text('username',null,['class'=>'textbox_seach']) !!}     
     </div>
     <div class="login-field-space">
-        {!! Form::label('password', 'Password : ') !!}
-        {!! Form::password('password'); !!}      
+        {!! Form::label('password', 'Password : ',['class'=>'label_search','style'=>'width:80px;']) !!}
+        {!! Form::password('password',['class'=>'textbox_seach']); !!}      
     </div>
 
     <div class="login-field-space, login-submit">
-        {!! Form::submit('Log In') !!}
+        {!! Form::submit('Log In',['class'=> 'main_button']) !!}
     </div>
 
     {!! Form::close() !!}
