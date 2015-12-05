10d14eadc384aa8af59b29bdbc679ff300ee4f41
linhngoc1841 <linhngoc1841@yahoo.com>
2015-12-04 21:15:36 -0500

    post recipe

--- app/Http/Controllers/PostRecipeController.php
+++ app/Http/Controllers/PostRecipeController.php
@@ -8,6 +8,14 @@ use App\recipe;
 use App\step;
 use App\recipe_ingre;
 use Request;
+use Auth;
+use App\comment;
+use App\Made;
+use App\Vote;
+use Input;
+
+use \Illuminate\Support\Facades\Redirect;
+
 
 class PostRecipeController extends Controller {
 
@@ -17,8 +25,8 @@ class PostRecipeController extends Controller {
         $this->middleware('auth');
     }
 
-    public function getIndex($id = null) {
-
+    public function getIndex() {
+        $id= Request::input('id');
         $ingredients = ingredient::all();
         $ingredientArr = array();
         foreach ($ingredients as $ingredient) {
@@ -26,57 +34,127 @@ class PostRecipeController extends Controller {
         }
         
         $recipe = recipe::find($id);
-        $recipeIngredients=null;
+        $recipeIngredients = null;
         $steps=null;
         if($recipe != null){
-            $recipeIngredients = recipe_ingre::where('id',$recipe->id);
-            $steps = step::where('id',$recipe->id);
+            $recipeIngredients = recipe_ingre::where('recipeid',$recipe->id)->get();
+            $steps = Step::where('recipeid', $recipe->id)->orderBy('steporder','ASC')->get();
         }
         
         return view("pages.postRecipe",['recipe'=>$recipe,'recipeIngredients'=>$recipeIngredients,'steps'=>$steps,'ingredients'=>$ingredientArr]);
     }
 
-    public function getDeleteRecipe($id) {
+    public function postAddrecipe() {      
+        $recipe = new recipe();
+        $recipe->name = Request::input('name');
+        $recipe->description = Request::input('description');
+        $recipe->servings = Request::input('servings');
+        $recipe->datepost = date('Y-m-d H:i:s',time());
+        $recipe->userpostid = Auth::user()->id;
+        
+        $file = Input::file('img');
+
+        if (Input::hasFile('img') && $file->isValid()) {     
+            $imgFolder = 'assets/images/article_pic';
+            $extension = $file->getClientOriginalExtension();
+            $fileName = "recipe_user". Auth::user()->id .rand (1,100 ). date("YmdHis", time()) . '.' . $extension;
+            $file->move($imgFolder, $fileName); // uploading file to given path
+            $recipe->img = $fileName;
+        }
+        $recipe->save();
+        return redirect('/postRecipe?id=' . $recipe->id);
+    }
+    
+    public function getDeleterecipe($id) {
+        Vote::where('recipeid', $id)->delete();
+        Made::where('recipeid', $id)->delete();
+        step::where('recipeid', $id)->delete();
+        recipe_ingre::where('recipeid', $id)->delete();
+        comment::where('recipeid', '=', $id)->delete();
+        recipe::find($id)->delete();
+        return  redirect('/userprofile/' .Auth::user()->username);
         
     }
 
-    public function postAdd() {
-        if (Request::ajax()) {
-            $date = new DateTime();
-            $recipe = new recipe();
-            $recipe->name = Request::input('name');
-            $recipe->description = Request::input('description');
-            $recipe->servings= Request::input('servings');
-            $recipe->image=Request::input('image');
-           // $recipe->datepost= $date;
-            $recipe->id=  Auth::user()->id;
-            $recipe->save();
-
-            $last_recipe = $recipe->id;
-
-            $recipes = recipe::whereId($last_recipe)->get();
-            return "OK";
-        }
-    }
-
-    public function postUpdateRecipe($id) {
-
-        if (Request::ajax()) {
-
-            $recipe = recipe::find($id);
-            $this->setData($recipe);
-            $recipe->save();
-            return "OK";
-        }
-    }
-
-    public function setData($recipe) {
+    public function postUpdaterecipe($id) {
+        $recipe = recipe::find($id);
+        $inputs = Input::all();
+        $recipe->name = $inputs['name'];
+        $recipe->description = $inputs['description'];
+        $recipe->servings = $inputs['servings'];
+        $recipe->datepost = date('Y-m-d H:i:s',time());
         $recipe->userpostid = Auth::user()->id;
-        $recipe->datepost = date("Y-m-d");
-        $recipe->name = Request::input('name');
-        $recipe->description = Request::input('description');
-        $recipe->image = Request::input('image');
-        $recipe->servings = Request::input('servings');
+        
+        $file = Input::file('img');
+
+        if (Input::hasFile('img') && $file->isValid()) {     
+            $imgFolder = 'assets/images/article_pic';
+            $extension = $file->getClientOriginalExtension();
+            $fileName = "recipe_".$id .rand (1,100 ). date("YmdHis", time()) . '.' . $extension;
+            $file->move($imgFolder, $fileName); // uploading file to given path
+            $recipe->img = $fileName;
+       }
+       
+        $recipe->save();
+        return redirect('/postRecipe?id=' . $id);
+    }
+    
+    public function getAddingredient($recipeid) {      
+        $recipeIngredient = new recipe_ingre();
+        $recipeIngredient->recipeid = $recipeid;
+        if(ingredient::all() != null)
+            $recipeIngredient->ingredientid = ingredient::all()->first()->id;
+        $recipeIngredient->save();
+        return redirect('/postRecipe?id=' . $recipeid);
+    }
+    
+    public function getDeleteingredient($id) {
+        $recipeIngredient = recipe_ingre::find($id);
+        $recipeId = $recipeIngredient->recipeid;
+        $recipeIngredient->delete();
+        return redirect('/postRecipe?id=' . $recipeId);
     }
 
+    public function postUpdateingredient($id) {
+        $recipeIngredient = recipe_ingre::find($id);
+        $recipeIngredient->ingredientid = Request::input('ingredientId');
+        $recipeIngredient->detail = Request::input('detail');
+        $recipeIngredient->save();
+        return $recipeIngredient->recipeid;
+    }
+    
+    public function getAddstep($recipeid) {      
+        $step = new step();
+        $step->recipeid = $recipeid;
+        $step->save();
+        return redirect('/postRecipe?id=' . $recipeid);
+    }
+    
+    public function getDeletestep($id) {
+        $step = step::find($id);
+        $recipeId = $step->recipeid;
+        $step->delete();
+        return redirect('/postRecipe?id=' . $recipeId);
+    }
+
+    public function postUpdatestep($id) {
+        $step = step::find($id);
+        $inputs = Input::all();
+        $step->content = $inputs['content' . $id];
+        $step->steporder = $inputs['steporder'. $id];
+        $file = Input::file('picture' . $id);
+ 
+        if (Input::hasFile('picture' . $id) && $file->isValid()) {     
+            $imgFolder = 'assets/images/article_pic';
+            $extension = $file->getClientOriginalExtension();
+            $fileName = "step_recipe".$id .rand (1,100 ). date("YmdHis", time()) . '.' . $extension;
+            $file->move($imgFolder, $fileName); // uploading file to given path
+            $step->picture = $fileName;
+       }
+        
+        $step->save();
+        return redirect('/postRecipe?id=' . $step->recipeid);
+    }
+
+    
 }
